5297603fc4ed10dfea326bf7bd52044ded140ea7
Mridul00700 <55996869+Mridul00700@users.noreply.github.com>
2021-11-05 23:40:57 +0530

    newTask- using cutom hook

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import useApi from '../hooks/api-task';
 
 import Section from '../UI/Section';
@@ -7,16 +7,30 @@ import TaskForm from './TaskForm';
 const NewTask = (props) => {
   // const [isLoading, setIsLoading] = useState(false);
   // const [error, setError] = useState(null);
-  useApi();
+  const { sendRequest: sendTaskRequest, loading: isLoading, error } = useApi();
+
+  const createTask = (taskText, data) => {
+    const generatedId = data.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  }
+
 
   const enterTaskHandler = (taskText) => {
 
 
+    sendTaskRequest({
+      url: 'Your Firebase URL',
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json', },
+      body: { text: taskText }
+    }, createTask.bind(null, taskText)); // Pre-configuring the createTask to include taskText
     // setIsLoading(true);
     // setError(null);
     // try {
     //   const response = await fetch(
-    //     'https://react-http-76887-default-rtdb.firebaseio.com/task.json',
+    //     'Your firebase URL',
     //     {
     //       method: 'POST',
     //       body: JSON.stringify({ text: taskText }),
